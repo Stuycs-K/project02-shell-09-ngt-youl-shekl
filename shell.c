f00513ba1d5b35dccca0dfcebd4ad47c4c9e1819
lawrence <lshek50@stuy.edu>
2024-12-03 14:50:34 -0500

    exec somewhat working

--- shell.c
+++ shell.c
@@ -10,6 +10,29 @@
 #include <dirent.h>
 #include "functions.h"
 
+
+void run_cmd(char **args) {
+  pid_t p = fork();
+  if (p < 0) {
+    perror("fork failed");
+    exit(1);
+  }
+  else if (p == 0) {
+    execvp(args[0], args);
+  }
+  else {
+    // parent
+    int status;
+    pid_t childPid = wait(&status);
+    if (childPid == -1) {
+      perror("wait error");
+      exit(1);
+    }
+    // printf("command done\n");
+  }
+}
+
+
 int main() {
     while (1) {
         // char** cmd_list[256];
@@ -21,10 +44,7 @@ int main() {
         for (int i = 0; cmd[i] != NULL; i++) {
             char* args[256];
             parse_args(cmd[i], args);
-            // fork and execvp each command
-            fork();
-            
-            execvp(args[0], args);
+            run_cmd(args);
         }
     }
 }
