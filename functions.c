85dbed7a46c375fd1a080bed7ac00da59eb1053e
lawrence <lshek50@stuy.edu>
2024-12-04 13:49:37 -0500

    removed testing print messages

--- functions.c
+++ functions.c
@@ -76,8 +76,6 @@ void run_cd(char ** args) {
     if (getcwd(curr_dir, sizeof(curr_dir)) == NULL) {
         perror("curr getcwd() error\n");
     }
-    printf("curr dir : %s\n", curr_dir);
-    printf("change dir : %s\n", args[1]);
     if (chdir(args[1]) != 0) {
         perror("chdir() failed\n");
     }
@@ -85,5 +83,4 @@ void run_cd(char ** args) {
     if (getcwd(new_dir, sizeof(new_dir)) == NULL) {
         perror("new getcwd() error\n");
     }
-    printf("new dir : %s\n", new_dir);
 }
